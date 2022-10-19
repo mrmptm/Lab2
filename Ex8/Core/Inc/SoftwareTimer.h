9cfd091a39c5371d9df9f1a43fa565dac9c7bbe7
unknown <lminhphuctm789@gmail.com>
2022-10-19 18:38:54 +0700

    first commit

---
+++ Ex8/Core/Inc/SoftwareTimer.h
@@ -0,0 +1,19 @@
+/*
+ * SoftwareTimer.h
+ *
+ *  Created on: Sep 28, 2022
+ *      Author: DELL
+ */
+
+#ifndef SOFTWARETIMER_H_
+#define SOFTWARETIMER_H_
+
+extern int counter1;
+extern int timer1_flag;
+void setTimer1(int duration);  //millisecond
+extern int counter2;
+extern int timer2_flag;
+void setTimer2(int duration);  //millisecond
+void TimerRun();
+
+#endif /* SOFTWARETIMER_H_ */
---
+++ Ex8/Core/Src/SoftwareTimer.c
@@ -0,0 +1,42 @@
+/*
+ * SoftwareTimer.c
+ *
+ *  Created on: Sep 28, 2022
+ *      Author: DELL
+ */
+#include "SoftwareTimer.h"
+int counter1;
+int timer1_flag;
+
+int counter2;
+int timer2_flag;
+
+int TIMER_CYCLE=10;
+
+void setTimer1(int duration){
+	counter1=duration/TIMER_CYCLE;
+	timer1_flag=0;
+}
+
+void setTimer2(int duration){
+	counter2=duration/TIMER_CYCLE;
+	timer2_flag=0;
+}
+
+void TimerRun(){
+	if(counter1>0){
+		counter1--;
+		if(counter1<=0){
+			timer1_flag=1;
+		}
+	}
+	if(counter2>0){
+		counter2--;
+		if(counter2<=0){
+			timer2_flag=1;
+		}
+	}
+
+}
+
+
